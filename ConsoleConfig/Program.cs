c3967f17c52625a6c59730e4a3f36de49f91705f
Anand Neema <anand.neema14@gmail.com>
2025-01-29 16:45:57 +0530

    Config Hierarchy

---
+++ ConsoleConfig/Program.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace ConsoleConfig;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var Name = "Anand";
+        IConfigurationRoot configurationRoot = new ConfigurationBuilder()
+            .AddJsonFile("config.json") //This is adding config file dynamically
+            .AddEnvironmentVariables()  //This we mainly use in docker files or during containerization
+            .AddCommandLine(args)
+            .AddUserSecrets(typeof(Program).Assembly)   //The value is stored in JSON file in the local machine's user profile folder
+            .Build();
+        Console.WriteLine($"{configurationRoot["Greeting"]},{Name}!");
+        Console.WriteLine($"Configuration: {configurationRoot["Environment"]}!");
+        Console.ReadKey();
+    }
+}
