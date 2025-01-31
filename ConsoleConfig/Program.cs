a9e628e54de59823567b45485c19ab480643adfe
anand.neema14@gmail.com <anand.neema14@gmail.com>
2025-01-31 15:05:57 +0530

    Added Specter Library for beautifying the console, and also added parameters for comman line args

--- ConsoleConfig/Program.cs
+++ ConsoleConfig/Program.cs
@@ -1,19 +1,27 @@
 ﻿using Microsoft.Extensions.Configuration;
+using Spectre.Console;
 
 namespace ConsoleConfig;
 
 class Program
-{
+{    
     static void Main(string[] args)
     {
+        var mappings = new Dictionary<string, string>()
+        {
+            {"--greeting","Greeting:Message" },
+            {"--color","Greeting:Color" },
+            { "--env","Environment" }
+        };
+
         var Name = "Anand";
         IConfigurationRoot configurationRoot = new ConfigurationBuilder()
             .AddJsonFile("config.json") //This is adding config file dynamically
             .AddEnvironmentVariables()  //This we mainly use in docker files or during containerization
-            .AddCommandLine(args)
             .AddUserSecrets(typeof(Program).Assembly)   //The value is stored in JSON file in the local machine's user profile folder
+            .AddCommandLine(args, mappings)
             .Build();
-        Console.WriteLine($"{configurationRoot["Greeting"]},{Name}!");
+        AnsiConsole.Markup($"[{configurationRoot["Greeting:Color"]}]{configurationRoot["Greeting:Message"]}[/],{Name}!");
         Console.WriteLine($"Configuration: {configurationRoot["Environment"]}!");
         Console.ReadKey();
     }
